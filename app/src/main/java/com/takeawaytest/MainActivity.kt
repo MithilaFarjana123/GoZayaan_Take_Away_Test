55239a6eb8b0f41fd4349275f94661757f7bfd1e
“Mithila <mithila.farjana.upoma@gmail.com>
2024-02-19 11:59:52 +0600

    splashscreen added

--- app/src/main/java/com/takeawaytest/MainActivity.kt
+++ app/src/main/java/com/takeawaytest/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.takeawaytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,11 +20,19 @@ import java.util.ArrayList
 class MainActivity : AppCompatActivity() {
 
 
-
+    private val SPLASH_DISPLAY_LENGTH = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textSplash: TextView = findViewById(R.id.textSplash)
 
+        // Optionally, you can customize the text or other properties here
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this@MainActivity, ContactList::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
 
     }
 
